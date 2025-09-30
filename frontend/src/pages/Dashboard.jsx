c80113ca20325e8837e1e83831e2216ad3e10d86
Stephen McMahon <stephen.mcmahon@ucdconnect.ie>
2025-09-30 22:07:09 +0100

    updated baseurl to azure vm

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Modal, Button } from "react-bootstrap";
 
 const api = axios.create({
-  baseURL: "",
+  baseURL: "http://20.199.81.36",
   headers: {
     "Content-Type": "application/json",
   },
